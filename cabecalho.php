7c94b1134474a6ee8df4c9367320887a8326efc3
aerocreed <aerocreed@users.noreply.github.com>
2017-02-19 09:57:19 -0300

    Add files via upload

--- cabecalho.php
+++ cabecalho.php
@@ -1,10 +1,8 @@
 <html>
     <head>
-<<<<<<< HEAD
         <title>Avaliacao Premiada</title>
-=======
-        <title>Minha Loja</title>
->>>>>>> 965b0df20fa14d0f532f98b3947518c7dc38f870
+
+        <title>Avalição Premiada</title>
         <meta charset="utf-8"></meta>
         <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'/>
         <link href="/css/geral.css" rel="stylesheet">
@@ -15,19 +13,11 @@
         <div class="navbar navbar-inverse">
             <div class="container">
                 <div class="navbar-header">
-<<<<<<< HEAD
                     <a class="navbar-brand" href="/facebook_post/index.php"> Inicio </a>
                 </div>
                 <div>
                     <ul class="nav navbar-nav">
-                        <li><a href="/facebook_post/avaliar.php">Avaliação</a></li>
-=======
-                    <a class="navbar-brand" href="/avaliacao-premiada/index.php"> Home </a>
-                </div>
-                <div>
-                    <ul class="nav navbar-nav">
-                        <li><a href="/avaliacao-premiada/Avaliacao/AvaliacaoView/formoid_verde1.php">Avaliação</a></li>
->>>>>>> 965b0df20fa14d0f532f98b3947518c7dc38f870
+                        <li><a href="/facebook_post/index2.php">Avaliação</a></li>
                         <li><a href="relatorio.php">Relatório</a></li>
                         <li><a href="sobre.php">Sobre</a></li>
                     </ul>
--- post.php
+++ post.php
@@ -1,8 +1,4 @@
 <?php
-$opiniao = $_POST['opiniao'];
-
-print_r($opiniao);
-
 date_default_timezone_set('UTC');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
